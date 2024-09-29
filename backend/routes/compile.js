eaf6fc30c6e4afd7776fee0d23470de4aef1b008
Adityasiig <adityaajaysingh0104@gmail.com>
2024-09-29 11:29:52 +0530

    Create compile.js

---
+++ backend/routes/compile.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { compileCode } = require('../controllers/compilerController');
+const router = express.Router();
+
+router.post('/', compileCode);
+
+module.exports = router;
